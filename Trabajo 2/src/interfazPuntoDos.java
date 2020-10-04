78f63071e7ac19de3ded07fd61cdd65ff47766f9
Epalaciol <esteban.1054@hotmail.com>
2020-10-04 01:12:37 -0500

    Add clase Conexion

--- Trabajo 2/src/interfazPuntoDos.java
+++ Trabajo 2/src/interfazPuntoDos.java
@@ -1,14 +1,21 @@
-import javax.swing.*;
-
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-
-import java.awt.*;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.*;
-import java.math.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 public class interfazPuntoDos extends Container implements ActionListener {
 
@@ -67,24 +74,24 @@ public class interfazPuntoDos extends Container implements ActionListener {
     public void actionPerformed(final ActionEvent e) {
         // TODO Auto-generated method stub
         if (e.getActionCommand().equals("Insertar")) {
+
+            // QUITAR CCOMENTARIOS AL FINAL
+            // if (coordenadas.getText().isEmpty() ||
+            // ciudad.getSelectedItem().toString().isEmpty()
+            // || vendedor.getText().isEmpty()) {
+            //
+            // JOptionPane.showMessageDialog(null, "Hay campos que aun no se han
+            // diligenciado ", "Missing Data",
+            // JOptionPane.INFORMATION_MESSAGE);
+            //
+            // } else {
+            //
+            // }
             try {
                 insertarDatos(coordenadas.getText(), ciudad.getSelectedItem().toString(),
                         Integer.parseInt(vendedor.getText()));
             } catch (SQLException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-                /*
-                 * if (coordenadas.getText().isEmpty() ||
-                 * ciudad.getSelectedItem().toString().isEmpty() ||
-                 * vendedor.getText().isEmpty()) { JOptionPane.showMessageDialog(null,
-                 * "Hay campos que aun no se han diligenciado ", "Missing Data",
-                 * JOptionPane.INFORMATION_MESSAGE);
-                 * 
-                 * } else {
-                 * 
-                 * 
-                 * }
-                 */
+
             }
 
         } else {
@@ -94,28 +101,14 @@ public class interfazPuntoDos extends Container implements ActionListener {
     }
 
     public void listarCiudades() throws SQLException {
-
-        Connection conn;
+        
+        Connection conn = Conexion.dbConexion();
         Statement sentencia = null;
         ResultSet resultado;
 
-        try { // Se carga el driver JDBC-ODBC
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+        sentencia = conn.createStatement();
 
-        } catch (Exception e1) {
-            System.out.println("No se pudo cargar el driver JDBC");
-
-        }
-
-        try { // Se establece la conexión con la base de datos Oracle Express
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@LAPTOP-ANL3RGG0:1521:xe", "epalaciol", "admin");
-            sentencia = conn.createStatement();
-
-        } catch (SQLException e1) {
-            System.out.println("No hay conexión con la base de datos.");
-
-        }
-
+       
         // Consulta para extraer la ciudades almacenadas en la tabla City
         try {
             resultado = sentencia.executeQuery("SELECT Nombre_ciudad FROM CITY ORDER BY Nombre_Ciudad ASC");
@@ -126,31 +119,19 @@ public class interfazPuntoDos extends Container implements ActionListener {
             // TODO Auto-generated catch block
             System.out.println("No se pudo realizar la consulta");
         }
+        conn.commit();
+        conn.close();
 
     }
 
     public void insertarDatos(String coordenadasXY, String nombreCiudad, Integer codigoVendedor) throws SQLException {
 
-        Connection conn;
+        Connection conn = Conexion.dbConexion();
         Statement sentencia = null;
         ResultSet resultado;
 
-        try { // Se carga el driver JDBC-ODBC
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-
-        } catch (Exception e1) {
-            System.out.println("No se pudo cargar el driver JDBC");
-
-        }
-
-        try { // Se establece la conexión con la base de datos Oracle Express
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@LAPTOP-ANL3RGG0:1521:xe", "prueba", "admin");
-            sentencia = conn.createStatement();
-
-        } catch (SQLException e1) {
-            System.out.println("No hay conexión con la base de datos.");
-
-        }
+       
+        sentencia = conn.createStatement();        
 
         // Consulta para extraer la ciudades almacenadas en la tabla City
         try {
@@ -163,20 +144,18 @@ public class interfazPuntoDos extends Container implements ActionListener {
             System.out.println("No se pudo realizar la consulta");
         }
 
-        
-
         String formatoCoordenadas = coordenadasXY.replaceAll("\s", "");
         formatoCoordenadas = formatoCoordenadas.replace("\n", ",");
         String puntosVentas[] = formatoCoordenadas.split(",");
 
-        HashMap<String, Double> validacion = new HashMap<>();
+        HashMap<String, Integer> validacion = new HashMap<>();
 
         if (puntosVentas.length % 3 == 0) {
 
             for (int i = 0; i < puntosVentas.length; i += 3) {
 
                 String clave = puntosVentas[i] + "," + puntosVentas[i + 1];
-                Double valor = Double.parseDouble(puntosVentas[i + 2]);
+                Integer valor = Integer.parseInt(puntosVentas[i + 2]);
 
                 if (validacion.containsKey(clave)) {
                     validacion.put(clave, validacion.get(clave) + valor);
@@ -186,42 +165,52 @@ public class interfazPuntoDos extends Container implements ActionListener {
                 }
 
             }
+
+            ArrayList<Integer> coordenadasInsertar = new ArrayList<>();
+
+            for (String key : validacion.keySet()) {
+                String clave[] = key.split(",");
+                Integer valor = validacion.get(key);
+                coordenadasInsertar.add(Integer.parseInt(clave[0]));
+                coordenadasInsertar.add(Integer.parseInt(clave[1]));
+                coordenadasInsertar.add(valor);
+            }
+            try {
+                String anidada = "nest_ventas(ventas_tip(";
+                int contador = 0;
+                for (int i = 0; i < coordenadasInsertar.size(); i++) {
+                    if (contador == 2) {
+                        anidada += coordenadasInsertar.get(i) + ")";
+                        if (i != coordenadasInsertar.size() - 1) {
+                            anidada += ", ventas_tip(";
+                        }
+                        contador = 0;
+                    } else {
+                        anidada += coordenadasInsertar.get(i) + ",";
+                        contador += 1;
+                    }
+
+                }
+                anidada += "))";
+                sentencia.executeQuery(
+                        "INSERT INTO VVCITY VALUES(" + codigoVendedor + ",'" + nombreCiudad + "'," + anidada);
+
+                JOptionPane.showMessageDialog(null,
+                        "Los datos suministrados han sido añadidos correctamente",
+                        "Datos Insertados", JOptionPane.INFORMATION_MESSAGE);
+            }
+
+            catch (SQLException e1) {
+                if (e1.getErrorCode() == 1) {
+                    System.out.println("Funciona");
+                }
+            }
+
         } else {
             // JOptionPane.showMessageDialog(null, "Las los datos suministrados de la
             // ubicación de las ventas al parecer estan incompletos ", "Missing Data",
             // JOptionPane.INFORMATION_MESSAGE);
             // System.out.println("Las coordenadas estan incompletas");
-        }
-
-        try {
-            String anidada = "nest_ventas(ventas_tip(";
-            int contador = 0;
-            for (int i = 0; i < 12; i++) {
-                if(contador == 2){
-                    anidada += i+")";
-                    if(i != 11){
-                        anidada +=", ventas_tip(";
-                    }
-                    contador = 0;
-                }
-                else {
-                    anidada  += i+",";
-                    contador += 1;
-                }
-                
-                
-
-            }
-            anidada += "))";
-            System.out.println(anidada);
-            sentencia.executeQuery("INSERT INTO VVCITY VALUES(" + codigoVendedor + ",'" + nombreCiudad + "',"
-                    + anidada);
-        }
-
-        catch (SQLException e1) {
-            if (e1.getErrorCode() == 1) {
-                System.out.println("Funciona");
-            }
         }
 
         // Setear los valores de la GUI en blanco
